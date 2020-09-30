c8369a8934c9384ba4ae16ec2e596eb8c1c1e43b
David Allen <david@inthecode.com.au>
2020-09-30 12:11:29 +1000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// @ts-check
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'search-app';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('searchInput') input: ElementRef;
+
+  public user: any;
+
+  constructor(private http: HttpClient) {}
+
+  public ngAfterViewInit(): void {
+    fromEvent<any>(this.input.nativeElement, 'keyup')
+      .pipe(
+        map(ev => ev.target.value),
+        debounceTime(400),
+        distinctUntilChanged(),
+        switchMap((user) => this.http.get(`https://jsonplaceholder.typicode.com/users/${user}`)),
+      ).subscribe(user => {
+        this.user = user;
+      });
+  }
 }
